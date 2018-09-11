839b04c3a6c4018ce391a1a91ddf31514a731a79
Sam Supplee-Niederman <niederman.sam@gmail.com>
2018-09-11 16:37:19 -0600

    some changes

--- P5_Empty/sketch.js
+++ P5_Empty/sketch.js
@@ -1,44 +1,9 @@
 
-var angle = TWO_PI/3;
-var slider;
 
 function setup() {
   createCanvas(800, 800);
-  slider = createSlider(PI / 6, TWO_PI, PI / 3, 0.01);
 }
 
 function draw() {
   background(51);
-  angle = slider.value();
-  stroke(255);
-  translate(width/2, height/2);
-  push();
-  branch(200);
-  pop();
-  push();
-  rotate(angle*2);
-  branch(200);
-  pop();
-  push();
-  rotate(-angle*2);
-  branch(200);
-  pop();
-  push();
-  rotate(-angle*4);
-  branch(200);
-}
-
-function branch(len) {
-  line(0, 0, 0, -len);
-  translate(0, -len);
-  if (len > 1) {
-    push();
-    rotate(angle);
-    branch(len * 0.6);
-    pop();
-    push();
-    rotate(-angle);
-    branch(len * 0.6);
-    pop();
-  }
 }
